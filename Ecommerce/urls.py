02da82c1c057d632be599a3ef3fe9984bb92d4e8
Kilian Olry <kilian.olry@gmail.com>
2023-05-10 18:25:16 +0200

    Correction du login

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -12,10 +12,12 @@ urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    path('signup/', signup, name='signup'),
-    path('login/', views.LoginView.as_view(template_name = 'core/login.html'), name='login'),
+
+    # Auth
     path('admin/', admin.site.urls),
+    path('signup/', signup, name='signup'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(template_name = 'core/login.html'), name='login'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
