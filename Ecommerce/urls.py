b81696cb50ae379cd74ad8e38be1cd956de2a4ae
Kilian Olry <kilian.olry@gmail.com>
2023-05-12 11:59:50 +0200

    New app orders + new database + new view + correct order in account

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('cart/', include('cart.urls')),
 
-
+    path('order/', include('order.urls')),
     path('admin/', admin.site.urls),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
